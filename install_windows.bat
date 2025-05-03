028c5de2e284ba98e9e979afec55ae748ba7d502
NameOfFool <ivan.pauk2008@gmail.com>
2025-05-03 18:25:11 +0300

    updated windows bat install

--- install_windows.bat
+++ install_windows.bat
@@ -8,22 +8,22 @@ set INSTALL_DIR=install
 set GENERATOR="Visual Studio 17 2022"
 set CONFIG=Release                      
 
-:: Очистка предыдущей сборки
+
 if exist %BUILD_DIR% rmdir /s /q %BUILD_DIR%
 if exist %INSTALL_DIR% rmdir /s /q %INSTALL_DIR%
 
-:: Создание директорий
+
 mkdir %BUILD_DIR%
 cd %BUILD_DIR%
 
-:: Генерация проекта
+
 cmake .. -G %GENERATOR% -DCMAKE_INSTALL_PREFIX=..\%INSTALL_DIR% 
 
-:: Компиляция (для MSVC)
+
 cmake --build . --config %CONFIG%
 cmake --install . --config %CONFIG%
 
-
+move .\Release\* ..\%INSTALL_DIR% 
 cd ..
 echo Success! Result saved in %INSTALL_DIR%/
 pause
