aad1333bf80f142f5809829df1e34da06419b078
NameOfFool <ivan.pauk2008@gmail.com>
2025-05-05 09:24:22 +0300

    updated linux install script

--- install_linux.sh
+++ install_linux.sh
@@ -1,11 +1,30 @@
 #/bin/sh
 
-rm -rf build
-rm -rf install
-mkdir install
-mkdir build && cd build
-cmake ..
-cd build
-cmake --build .
-find . -maxdepth 1 -type f -executable -exec bash -c 'mv $1 ../install' - {} \;
-echo "Success! result saved in install dir."
+interruptfn(){
+  trap SIGINT
+  cd ..
+  rm -rf build
+  rm -rf install
+  echo "Interrupted"
+  exit
+}
+echo "WARNING! IF YOU RUN THIS SCRIPT, YOU WILL DELETE build AND install FOLDERS"
+read -r -p "Proceed? [y/N]" response
+case "$response" in
+  [yY][eE][sS]|[yY])
+  rm -rf build
+  rm -rf install
+  mkdir install
+  mkdir build && cd build
+  trap "interruptfn" INT
+  cmake ..
+  cd build
+  cmake --build .
+  find . -maxdepth 1 -type f -executable -exec bash -c 'mv $1 ../install' - {} \;
+  echo "Success! result saved in install dir."
+  trap SININT
+  ;;
+*)
+  echo "Building was cancelled"
+  ;;
+esac
